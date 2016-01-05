91761ae7d5ef60f03aaa103c9d550331a0d6d4f0
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2016-01-05 17:11:47 +0100

    Bug Not showing error on Register password/email

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -7,12 +7,17 @@
     var register = this;
 
     FlashService.clear();
-    
+
     register.data = {};
 
     register.submit = function() {
       RegistrationService.register(register.data)
       .then(function(response) { // success
+        if(response.data.password ) {
+          FlashService.show(response.data.password);
+        } else if(response.data.email) {
+          FlashService.show(response.data.email);
+        }
         $location.path('/domains');
       });
     }
